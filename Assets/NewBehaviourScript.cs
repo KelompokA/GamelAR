398399471598a71b7aa3e302f55ea510fc459ff2
Luthfan Ubai <luthfan.ubai@live.com>
2016-05-02 11:55:09 +0700

    first commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+   
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+    
+    void OnMouseOver()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            AudioSource audio = GetComponent<AudioSource>();
+            audio.Play();
+            //audio.Play(44100);
+            //audio.PlayDelayed()
+            Debug.Log("AAAAAAAAAAAAaaaaaaaaaaaaaaaaaaaaaa");
+        }
+    }
+    
+}
