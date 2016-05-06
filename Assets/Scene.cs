425f0138c9f59ad9101b28c9212657cf22ee58ce
Luthfan Ubai <luthfan.ubai@live.com>
2016-05-06 23:48:42 +0700

    can link scene by a button, not clickable

--- Assets/Scene.cs
+++ Assets/Scene.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.EventSystems;
+//using UnityEngine.SceneManagement;
 
 public class Scene : MonoBehaviour {
 
@@ -10,11 +12,34 @@ public class Scene : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        
+	    if(Input.GetKeyDown(KeyCode.Space))Application.LoadLevel(1);
+//        
+//        if (Input.GetMouseButtonDown(0))
+//        {
+//            Application.LoadLevel(1);
+//        }
 	}
     
-    void MouseClick(){
+    public void onClick(){
     
-        SceneManager.LoadScene(0);
+//        SceneManager.LoadScene(1);
+        Application.LoadLevel(1);
     }
 }
+
+//
+//public class Scene : MonoBehaviour 
+// {
+//     public void NextSceneButton(int index)
+//     {
+//         Application.LoadLevel(index);
+//         Debug.Log("AADC");
+//     }
+// 
+//     public void NextSceneButton(string levelName)
+//     {
+//         Application.LoadLevel(levelName);
+//         Debug.Log("AADC");
+//     }
+// }
---
+++ Assets/Scene2.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+public class Scene2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+    void OnMouseOver()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Application.LoadLevel(1);
+        }
+    }
+}
