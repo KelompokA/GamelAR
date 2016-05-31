c8c41b2f6781dc0fd8da7cfce77f6b3d5ad139f0
IlhamEndianto <r.bayu.ilham.e@mail.ugm.ac.id>
2016-05-31 17:27:57 +0700

    fixing interface

--- Assets/Scene2.cs
+++ Assets/Scene2.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections;
 
 public class Scene2 : MonoBehaviour {
-
+	
 	// Use this for initialization
 	void Start () {
 	
@@ -13,11 +13,36 @@ public class Scene2 : MonoBehaviour {
 	
 	}
     
-    public void onClick(int sceneId)
+	public void onClick(int sceneId)
 	{
         Application.LoadLevel(sceneId);
-    }
+	}
+
+	public int cek = 0;
+	public int cekk= 0;
+	public void apapun()
+	{
+		if (cek==1)
+		{		GameObject.FindGameObjectWithTag ("HowTo").transform.position = new Vector2 (0, 1000);
+		cek=0;
+		}
+		else if (cek==0)
+		{		GameObject.FindGameObjectWithTag ("HowTo").transform.position = new Vector2 (417, 127);
+			cek= 1;
+		}
+	}
     
+	public void apapun1()
+	{
+		if (cekk==1)
+		{		GameObject.FindGameObjectWithTag ("AboutUs").transform.position = new Vector2 (0, 1000);
+			cekk=0;
+		}
+		else if (cekk==0)
+		{		GameObject.FindGameObjectWithTag ("AboutUs").transform.position = new Vector2 (417, 127);
+			cekk= 1;
+		}
+	}
 /*    public void OnPointerClick()
     {
         Application.LoadLevel(1);
